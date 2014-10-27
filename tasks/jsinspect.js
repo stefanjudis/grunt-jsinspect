4fad243bcf98f1d88d5ebf97d0d6c9c451e23e07
Juga Paazmaya <paazmaya@yahoo.com>
2014-10-27 17:14:56 +0200

    Bare structure for a Grunt plugin that only reads files

---
+++ tasks/jsinspect.js
@@ -0,0 +1,24 @@
+/*
+ * grunt-jsinspect
+ *
+ *
+ * Copyright (c) 2014 Stefan Judis
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var jsinspect = require('jsinspect');
+
+module.exports = function(grunt) {
+
+  grunt.registerMultiTask('jsinspect', 'Grunt task for jsinspect', function() {
+
+    var options = this.options({});
+
+    this.filesSrc.forEach(function(filepath) {
+
+    });
+    
+  });
+};
