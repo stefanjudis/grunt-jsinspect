d8f4015ae87b3b3844b1f3d2993780fcca166f2d
Juga Paazmaya <paazmaya@yahoo.com>
2014-10-27 18:40:41 +0200

    Started with command line options

--- tasks/jsinspect.js
+++ tasks/jsinspect.js
@@ -8,17 +8,41 @@
 
 'use strict';
 
-var jsinspect = require('jsinspect');
-
 module.exports = function(grunt) {
 
   grunt.registerMultiTask('jsinspect', 'Grunt task for jsinspect', function() {
 
-    var options = this.options({});
-
-    this.filesSrc.forEach(function(filepath) {
-
+    var options = this.options({
+      threshold: 30,
+      identifiers: false
     });
+
+    var done = this.async;
+
+    var args = [
+      '-t',
+      options.threshold
+    ];
+
+    if (options.identifiers) {
+      args.push('-i');
+    }
+
+    args.push(this.filesSrc.join(' '));
     
+    var opts = {
+      cmd: 'jsinspect',
+      args: args
+    };
+    grunt.util.spawn(opts, function(error, result, code) {
+      if (error) {
+        grunt.log.error(error);
+      }
+      grunt.log.writeln(result);
+      grunt.log.writeln(code);
+
+      done();
+    });
+
   });
 };
