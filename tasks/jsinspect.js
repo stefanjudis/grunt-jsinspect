d44fda9e93c204a692f05817768bf4b6a6563534
Juga Paazmaya <paazmaya@yahoo.com>
2014-10-28 11:51:32 +0200

    Reverse engineered API usage

--- tasks/jsinspect.js
+++ tasks/jsinspect.js
@@ -8,41 +8,27 @@
 
 'use strict';
 
+var Inspector = require('jsinspect/lib/inspector');
+var Reporter = require('jsinspect/lib/reporters');
+
 module.exports = function(grunt) {
 
   grunt.registerMultiTask('jsinspect', 'Grunt task for jsinspect', function() {
 
     var options = this.options({
       threshold: 30,
+      diff: false,
       identifiers: false
     });
 
-    var done = this.async;
-
-    var args = [
-      '-t',
-      options.threshold
-    ];
-
-    if (options.identifiers) {
-      args.push('-i');
-    }
-
-    args.push(this.filesSrc.join(' '));
-    
-    var opts = {
-      cmd: 'jsinspect',
-      args: args
-    };
-    grunt.util.spawn(opts, function(error, result, code) {
-      if (error) {
-        grunt.log.error(error);
-      }
-      grunt.log.writeln(result);
-      grunt.log.writeln(code);
-
-      done();
+    var inspector = new Inspector(this.filesSrc, {
+      threshold:   options.threshold,
+      diff:        options.diff,
+      identifiers: options.identifiers
     });
 
+    var reporterType = new Reporter.default(inspector, options.diff);
+
+    inspector.run();
   });
 };
